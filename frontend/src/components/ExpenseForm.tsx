45f699002b05a8e7cced5aabacac1f5c87583057
Paulene B. Marquez <paulenemarquez@gmail.com>
2026-08-21 12:55:16 +0800

    Prevent creation of expenses with future dates (BONUS-001)

--- frontend/src/components/ExpenseForm.tsx
+++ frontend/src/components/ExpenseForm.tsx
@@ -7,6 +7,7 @@ import { ExpenseFormData } from "../types";
 import { EXPENSE_CATEGORIES } from "../constants/categories";
 import { TextField, SelectBox, Button } from "../vibes";
 import { useExpenseForm } from "../hooks/useExpenseForm";
+import { formatDate } from "../utils/expenseUtils";
 
 interface ExpenseFormProps {
   initialData?: Partial<ExpenseFormData>;
@@ -85,6 +86,7 @@ export function ExpenseForm({
         value={formData.date}
         onChange={(e) => handleChange("date", e.target.value)}
         error={errors.date}
+        max={formatDate(new Date())}
         fullWidth
         required
       />
